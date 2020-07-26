f53a7f9354267e1d87548a909716a42129f30de5
Tran Trung Hieu <trunghieu2570@gmail.com>
2020-07-26 13:42:39 +0700

    remove unused content-types

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,14 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: 'cloudinary',
-    providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
-    },
-  },
-});
+if (process.env.NODE_ENV === 'production') {
+	module.exports = ({ env }) => ({
+	  upload: {
+		provider: 'cloudinary',
+		providerOptions: {
+		  cloud_name: env('CLOUDINARY_NAME'),
+		  api_key: env('CLOUDINARY_KEY'),
+		  api_secret: env('CLOUDINARY_SECRET'),
+		},
+	  },
+	});
+} else {
+	module.exports = {};
+}
